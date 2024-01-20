44318bb6b29da68a464ec10bb91b9eac7d31cc29
N1ko1a <ivanovicn98@gmail.com>
2024-01-20 23:15:11 +0100

    Dodao sam responsive elements

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -12,26 +12,26 @@ function NavBar() {
       <h1 className=" flex  justify-center font-semibold font-serif ease-in-out duration-500 m-5 text-4xl sm:text-5xl  md:text-7xl">
         The Newspaper
       </h1>
-      <div className="flex justify-evenly w-fit border-b-2 border-black">
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base md:text-base md:hover:text-xl">
+      <div className="flex flex-wrap justify-evenly w-fit border-b-2 border-black ">
+        <button className="m-2 text-xs  hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base md:text-base md:hover:text-xl">
           GENERAL
         </button>
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base md:text-base md:hover:text-xl">
+        <button className="m-2 text-xs hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base md:text-base md:hover:text-xl">
           BUSINESS
         </button>
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base md:text-base md:hover:text-xl">
+        <button className="m-2 text-xs hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base md:text-base md:hover:text-xl">
           ENTERTAINMENT
         </button>
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base md:text-base md:hover:text-xl">
+        <button className="m-2 text-xs hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base md:text-base md:hover:text-xl">
           HEALTH
         </button>
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base md:text-base md:hover:text-xl">
+        <button className="m-2 text-xs hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base md:text-base md:hover:text-xl">
           SCIENCE
         </button>
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base md:text-base md:hover:text-xl">
+        <button className="m-2 text-xs hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base md:text-base md:hover:text-xl">
           SPORTS
         </button>
-        <button className="m-3  hover:font-bold w-fit h-10 ease-in-out duration-500 sm:text-xs sm:hover:text-base  md:text-base md:hover:text-xl">
+        <button className="m-2 text-xs hover:font-bold w-fit h-5 ease-in-out duration-500 sm:text-xs sm:m-3 sm:hover:text-base  md:text-base md:hover:text-xl">
           TECHNOLOGY
         </button>
       </div>
@@ -41,15 +41,15 @@ function NavBar() {
             type="text"
             placeholder="Search"
             value={search}
-            className="w-64 h-8 bg-gray-50 border-b-2 border-gray-600 rounded-xl  outline-none text-white pl-4 hover:bg-gray-200   transition duration-500 ease-in-out hover:text-black"
+            className="text-sm w-52 h-8 bg-gray-50 border-b-2 border-gray-600 rounded-xl  outline-none text-white pl-4 hover:bg-gray-200   transition duration-500 ease-in-out hover:text-black sm:w-64 sm:text-sm md:text-base"
             onChange={handleInputChange} // Update search results as the user types
           />
         </div>
         <div className="">
-          <button className="bg-gray-50 border-b-2 border-gray-600 h-8 w-20 m-2 rounded-xl hover:bg-gray-200 transition duration-500 ease-in-out drop-shadow-2xl">
+          <button className="text-sm bg-gray-50 border-b-2 border-gray-600 h-8 w-20 m-2 rounded-xl hover:bg-gray-200 transition duration-500 ease-in-out drop-shadow-2xl sm:text-sm md:text-base">
             Sign in
           </button>
-          <button className="bg-gray-50 border-b-2 border-gray-600 h-8 w-20 m-2 rounded-xl hover:bg-gray-200 transition duration-500 ease-in-out">
+          <button className="text-sm bg-gray-50 border-b-2 border-gray-600 h-8 w-20 m-2 rounded-xl hover:bg-gray-200 transition duration-500 ease-in-out sm:text-sm md:text-base">
             Sign up
           </button>
         </div>
